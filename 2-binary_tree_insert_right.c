81cdc61a15640880d20f876851f29e85f041ebba
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2024-01-07 17:33:48 -0300

    Task 2 fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -17,12 +17,16 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+
 	if (parent->right != NULL)
 	{
-		parent->right = NULL;
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
 	}
+	else
+		new_node->right = NULL;
 	parent->right = new_node;
-	new_node->parent = parent;
-
 	return (new_node);
 }
