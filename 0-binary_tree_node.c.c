b733d9be732df263df1aa1085a5ecd07e9047fb8
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2024-01-07 15:45:39 -0300

    first commit binary trees.h and 0

---
+++ 0-binary_tree_node.c.c
@@ -0,0 +1,23 @@
+#include "binary_trees_h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_tree;
+
+	if (value == NULL)
+	{
+		return(NULL);
+	}
+
+	new_tree->parent = parent;
+	new_tree->n = value;
+	new_tree->left = NULL;
+	new_tree->right = NULL;
+	return (new_tree);
+}
