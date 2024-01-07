11013b72281a800f025e6c93f9fde9a0f1ae0d14
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2024-01-07 16:32:05 -0300

    add parent

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,6 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
     new_node = binary_tree_node(parent, value);
 	parent->left = new_node;
 	new_node->parent = parent;
+    parent->left->parent = parent;
 
 	return (new_node);
 }
