5a88c8aa43b71af9ee89f3b1aa40aaec958e365e
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2024-01-07 16:23:40 -0300

    fix task1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,11 +16,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
+	new_node = malloc(sizeof(binary_tree_t));
     if (new_node == NULL)
 		return(NULL);
 
-	parent->left = new_node;
 	new_node->n = value;
+	parent->left = new_node;
+    new_node->parent = parent;
 
 	return (new_node);
 }
