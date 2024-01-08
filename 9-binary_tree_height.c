8521d2e7230c64d8162118786719f9df02cb6a7e
Kiwizacxs <brian.deleon2016@outlook.com>
2024-01-08 15:00:37 +0000

    mesure the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,13 +3,19 @@
 size_t
 binary_tree_height(const binary_tree_t *tree)
 {
+    size_t rheight = 0;
+    size_t lheight = 0;
+
     if (tree == NULL)
         return (0);
-    size_t rheight = binary_tree_height(tree->right);
-    size_t lheight = binary_tree_height(tree->left);
+    
+    if (tree->right)
+        rheight = (1 + binary_tree_height(tree->right));
+    if (tree->left)
+        lheight = (1 + binary_tree_height(tree->left));
 
     if (rheight > lheight)
-        return (rheight + 1);
+        return (rheight);
     else
-        return (lheight + 1);
+        return (lheight);
 }
