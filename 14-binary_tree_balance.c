6a80df9690272a20653d18f646fcc5b839660103
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2024-01-08 14:16:55 -0300

    commit

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,19 @@
 #include "binary_trees.h"
 
+size_t
+binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	size_t rheight = (1 + binary_tree_height(tree->left));
+	size_t lheight = binary_tree_height(tree->left);
+
+	if (rheight > lheight)
+		return (rheight + 1);
+	else
+		return (lheight + 1);
+}
+
 /**
  * binary_tree_balance - function that measures the balance factor of a binary tree
  * @tree: tree
