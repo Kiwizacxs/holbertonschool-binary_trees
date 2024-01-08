03e84f8385d94603beb15004244bc48980727845
Kiwizacxs <brian.deleon2016@outlook.com>
2024-01-08 16:31:35 +0000

    binary tree size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,14 +3,14 @@
 size_t
 binary_tree_size(const binary_tree_t *tree)
 {
-    size_t rheight = 0;
+    size_t size = 0;
 
     if (tree == NULL)
         return (0);
     if (tree != NULL)
     {
-        rheight += binary_tree_size(tree->right);
-        rheight += binary_tree_size(tree->left);
+        size += binary_tree_size(tree->right);
+        size += binary_tree_size(tree->left);
     }
-    return (rheight + 1);
+    return (size + 1);
 }
