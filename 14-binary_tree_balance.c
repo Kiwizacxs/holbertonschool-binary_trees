16c160eb6bf275eeb71bb5df5c19de7503d5efc9
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2024-01-08 14:22:57 -0300

    TASK 14 Fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,15 +3,21 @@
 size_t
 binary_tree_height(const binary_tree_t *tree)
 {
+	size_t rheight = 0;
+	size_t lheight = 0;
+
 	if (tree == NULL)
 		return (0);
-	size_t rheight = (1 + binary_tree_height(tree->left));
-	size_t lheight = binary_tree_height(tree->left);
+
+	if (tree->right)
+		rheight = (1 + binary_tree_height(tree->right));
+	if (tree->left)
+		lheight = (1 + binary_tree_height(tree->left));
 
 	if (rheight > lheight)
-		return (rheight + 1);
+		return (rheight);
 	else
-		return (lheight + 1);
+		return (lheight);
 }
 
 /**
