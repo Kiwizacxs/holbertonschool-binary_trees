a02dfe4a85e4bf5b4ce0cc0fb64e2abf8c9c46ac
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2024-01-08 14:19:48 -0300

    task 14  fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -28,5 +28,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 	size_t left_height = binary_tree_height(tree->left);
 	size_t right_height = binary_tree_height(tree->right);
 
-	return ((int)(left_height - right_height));
+	return ((int)(right_height - left_height));
 }
