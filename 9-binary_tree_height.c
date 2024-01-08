b8e9d778b5546bd7ad70be284a95f18721c1b6a8
Kiwizacxs <brian.deleon2016@outlook.com>
2024-01-08 14:36:31 +0000

    mesure the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,7 +1,15 @@
-#include "binart_trees.h"
+#include "binary_trees.h"
 
 size_t
 binary_tree_height(const binary_tree_t *tree)
 {
-    
+    if (tree == NULL)
+        return (0);
+    size_t rheight = binary_tree_height(tree->right);
+    size_t lheight = binary_tree_height(tree->left);
+
+    if (rheight > lheight)
+        return (rheight + 1);
+    else
+        return (lheight + 1);
 }
