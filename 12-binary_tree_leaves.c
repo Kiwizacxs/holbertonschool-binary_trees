549b87d820691ae6250b5bcfefff9dd194c7549d
Kiwizacxs <brian.deleon2016@outlook.com>
2024-01-08 16:52:58 +0000

    nm of the leaves in a binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,15 +9,17 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t leaves = 0;
+
 	if (tree == NULL)
 		return 0;
 
 	if (tree->left == NULL && tree->right == NULL)
-		return 1;
+	{
+		return (1);
+	}
+		leaves += binary_tree_leaves(tree->left);
+		leaves += binary_tree_leaves(tree->right);
 
-	size_t left_leaves = binary_tree_leaves(tree->left);
-	size_t right_leaves = binary_tree_leaves(tree->right);
-
-
-	return left_leaves + right_leaves;
+	return (leaves);
 }
