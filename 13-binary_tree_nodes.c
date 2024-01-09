bac58623d4e871a143ce4a70e46c748b7678986d
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2024-01-09 10:50:18 -0300

    betty fix

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -3,14 +3,14 @@
 size_t
 binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t nodes = 0;
+	size_t nodes = 0;
 
 	if (tree == NULL)
 		return (0);
 
 	nodes += binary_tree_nodes(tree->left);
 	nodes += binary_tree_nodes(tree->right);
-    if (tree->left != NULL || tree->right != NULL)
+	if (tree->left != NULL || tree->right != NULL)
 	{
 		return (nodes + 1);
 	}
