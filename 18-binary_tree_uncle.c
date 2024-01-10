e51311bbc61627d36288db0b4896c7526a4a08ff
Kiwizacxs <brian.deleon2016@outlook.com>
2024-01-10 02:31:09 +0000

    uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+binary_tree_t
+*binary_tree_uncle(binary_tree_t *node)
+{
+    if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (node->parent->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (node->parent == node->parent->parent->right)
+	{
+		return (node->parent->parent->left);
+	}
+	else if (node->parent == node->parent->parent->left)
+	{
+		return (node->parent->parent->right);
+	}
+	return (NULL);
+}
+
