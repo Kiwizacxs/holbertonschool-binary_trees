1cc1f6e44f17b4540a83814595b469ba64d24f9c
Kiwizacxs <brian.deleon2016@outlook.com>
2024-01-10 02:32:09 +0000

    uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -3,7 +3,7 @@
 binary_tree_t
 *binary_tree_uncle(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
+	if (node == NULL || node->parent == NULL)
 	{
 		return (NULL);
 	}
@@ -21,4 +21,3 @@ binary_tree_t
 	}
 	return (NULL);
 }
-
