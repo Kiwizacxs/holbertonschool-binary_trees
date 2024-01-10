af2687e96f3b6892d90dec6f6f422c0b80a5146e
Kiwizacxs <brian.deleon2016@outlook.com>
2024-01-10 01:24:22 +0000

    sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+binary_tree_t
+*binary_tree_sibling(binary_tree_t *node)
+{
+	if (node->parent == NULL || node == NULL)
+		return (NULL);
+	
+	if (node->parent->left != node && node->parent->left != NULL)
+		return (node->parent->left);
+	else if (node->parent->right != node && node->parent->right != NULL)
+		return (node->parent->right);
+	else
+		return (NULL);  
+}
+
