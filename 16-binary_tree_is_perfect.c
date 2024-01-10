64fca42b7d439d069123ffff2caf52632e2a7513
Kiwizacxs <brian.deleon2016@outlook.com>
2024-01-10 00:40:17 +0000

    is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -5,7 +5,7 @@ binary_tree_is_perfect(const binary_tree_t *tree)
 {
     if (tree == NULL)
         return (0);
-    if (_binary_tree_balancion(tree) == 0 && _binary_tree_is_fut(tree) == 1)
+    if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1)
     {
         return (1);
     }
@@ -13,7 +13,7 @@ binary_tree_is_perfect(const binary_tree_t *tree)
         return (0);
 }
 
-int _binary_tree_balancion(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_height;
 	int right_height;
@@ -21,14 +21,14 @@ int _binary_tree_balancion(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = _binary_tree_lheig(tree);
-	right_height = _binary_tree_rightheig(tree);
+	left_height = binary_tree_lheigh(tree);
+	right_height = binary_tree_rheigh(tree);
 
 	return (right_height - left_height);
 }
 
 int
-_binary_tree_rightheig(const binary_tree_t *tree)
+binary_tree_rheigh(const binary_tree_t *tree)
 {
 	int rheight = 0;
 
@@ -36,13 +36,13 @@ _binary_tree_rightheig(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->right)
-		rheight = (1 + _binary_tree_rightheig(tree->right));
+		rheight = (1 + binary_tree_rheigh(tree->right));
 
 	return (rheight);
 }
 
 int
-_binary_tree_lheig(const binary_tree_t *tree)
+binary_tree_lheigh(const binary_tree_t *tree)
 {
 	int lheight = 0;
 
@@ -50,15 +50,13 @@ _binary_tree_lheig(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		lheight = (1 + _binary_tree_lheig(tree->left));
+		lheight = (1 + binary_tree_lheigh(tree->left));
 
 	return (lheight);
 }
 
-#include "binary_trees.h"
-
 int
-_binary_tree_is_fut(const binary_tree_t *tree)
+binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
@@ -69,8 +67,8 @@ _binary_tree_is_fut(const binary_tree_t *tree)
 	}
 	if (tree->left && tree->right)
 	{
-		return (_binary_tree_is_fut(tree->left) &&
-			_binary_tree_is_fut(tree->right));
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
 	}
 	return (0);
 }
